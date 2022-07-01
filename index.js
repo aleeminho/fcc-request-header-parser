da1cb70cf750debfe4f9bc6a9697f1cd3ef1079b
aleeminho <a.fikri2411@gmail.com>
2022-07-01 15:19:53 +0700

    finishing

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -19,13 +19,22 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
-
+app.get('/api/whoami', (req, res) => {
+  const software = req.headers['user-agent'];
+  const language = req.headers['accept-language'];
+  const ipaddress = req.headers['x-forwarded-for'] ?? req.connection.remoteAddress;
+  console.log(req.socket)
+  res.json({
+    ipaddress,
+    language,
+    software
+  });
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
